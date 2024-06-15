5aa8315cf762924ff0515a04f684dff80efd5f31
mrizkyff <muhamadrizkyff@gmail.com>
2024-06-15 19:00:12 +0700

    initial

---
+++ src/views/Login.jsx
@@ -0,0 +1,189 @@
+'use client'
+
+// React Imports
+import { useState } from 'react'
+
+// Next Imports
+import { redirect, useRouter } from 'next/navigation'
+
+// MUI Imports
+import Typography from '@mui/material/Typography'
+import TextField from '@mui/material/TextField'
+import IconButton from '@mui/material/IconButton'
+import InputAdornment from '@mui/material/InputAdornment'
+import Checkbox from '@mui/material/Checkbox'
+import Button from '@mui/material/Button'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Divider from '@mui/material/Divider'
+import axios from 'axios'
+
+// Third-party Imports
+import classnames from 'classnames'
+
+// Component Imports
+import Link from '@components/Link'
+import Logo from '@components/layout/shared/Logo'
+
+// Config Imports
+import themeConfig from '@configs/themeConfig'
+
+// Hook Imports
+import { useImageVariant } from '@core/hooks/useImageVariant'
+import { useSettings } from '@core/hooks/useSettings'
+import { Alert } from '@mui/lab'
+import SimpleBackdrop from '@components/SimpleBackdrop'
+
+const LoginV2 = ({ mode }) => {
+  // States
+  const [isPasswordShown, setIsPasswordShown] = useState(false)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [errorState, setErrorState] = useState(null)
+
+  // Vars
+  const darkImg = '/images/pages/auth-v2-mask-1-dark.png'
+  const lightImg = '/images/pages/auth-v2-mask-1-light.png'
+  const darkIllustration = '/images/illustrations/auth/v2-login-dark.png'
+  const lightIllustration = '/images/illustrations/auth/v2-login-light.png'
+  const borderedDarkIllustration = '/images/illustrations/auth/v2-login-dark-border.png'
+  const borderedLightIllustration = '/images/illustrations/auth/v2-login-light-border.png'
+
+  // Hooks
+  const router = useRouter()
+  const { settings } = useSettings()
+  const authBackground = useImageVariant(mode, lightImg, darkImg)
+
+  const characterIllustration = useImageVariant(
+    mode,
+    lightIllustration,
+    darkIllustration,
+    borderedLightIllustration,
+    borderedDarkIllustration
+  )
+
+  const handleClickShowPassword = () => setIsPasswordShown(show => !show)
+
+  const handleLogin = async (e) => {
+    setLoading(true)
+    e.preventDefault()
+    setErrorState(null)
+    try {
+      const loginResponse = await axios.post(process.env.NEXT_PUBLIC_API_URL + '/auth/login', {
+        username,
+        password
+      })
+      console.log(loginResponse.data)
+      localStorage.setItem('token', loginResponse.data.data)
+      redirect('/dashboard')
+    }
+    catch (e) {
+      setErrorState("Username dan password salah!")
+    }
+
+    setLoading(false)
+  }
+
+  return (
+    <div className='flex bs-full justify-center'>
+      {
+        loading && <SimpleBackdrop />
+      }
+      <div
+        className={classnames(
+          'flex bs-full items-center justify-center flex-1 min-bs-[100dvh] relative p-6 max-md:hidden',
+          {
+            'border-ie': settings.skin === 'bordered'
+          }
+        )}
+      >
+        <div className='pli-6 max-lg:mbs-40 lg:mbe-24'>
+          <img
+            src={characterIllustration}
+            alt='character-illustration'
+            className='max-bs-[673px] max-is-full bs-auto'
+          />
+        </div>
+        <img src={authBackground} className='absolute bottom-[4%] z-[-1] is-full max-md:hidden' />
+      </div>
+      <div className='flex justify-center items-center bs-full bg-backgroundPaper !min-is-full p-6 md:!min-is-[unset] md:p-12 md:is-[480px]'>
+        <div className='absolute block-start-5 sm:block-start-[38px] inline-start-6 sm:inline-start-[38px]'>
+          <Logo />
+        </div>
+        <div className='flex flex-col gap-5 is-full sm:is-auto md:is-full sm:max-is-[400px] md:max-is-[unset] mbs-11 sm:mbs-14 md:mbs-0'>
+          <div>
+            <Typography variant='h4'>{`Welcome to ${themeConfig.templateName}! 👋🏻`}</Typography>
+            <Typography className='mbs-1'>Please sign-in to your account and start the adventure</Typography>
+            {
+              errorState && (
+                <Alert icon={false} severity='error'>
+                  <Typography variant='body2'>{errorState}</Typography>
+                </Alert>
+              )
+            }
+          </div>
+          <form
+            noValidate
+            autoComplete='off'
+            onSubmit={(e) => handleLogin(e)}
+            className='flex flex-col gap-5'
+          >
+            <TextField autoFocus fullWidth label='Username' onChange={e => setUsername(e.target.value)} />
+            <TextField
+              fullWidth
+              label='Password'
+              type={isPasswordShown ? 'text' : 'password'}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position='end'>
+                    <IconButton
+                      size='small'
+                      edge='end'
+                      onClick={handleClickShowPassword}
+                      onMouseDown={e => e.preventDefault()}
+                    >
+                      <i className={isPasswordShown ? 'ri-eye-off-line' : 'ri-eye-line'} />
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }}
+              onChange={e => setPassword(e.target.value)}
+            />
+            <div className='flex justify-between items-center flex-wrap gap-x-3 gap-y-1'>
+              <FormControlLabel control={<Checkbox />} label='Remember me' />
+              <Typography className='text-end' color='primary' component={Link}>
+                Forgot password?
+              </Typography>
+            </div>
+            <Button fullWidth variant='contained' type='submit'>
+              Log In
+            </Button>
+            <div className='flex justify-center items-center flex-wrap gap-2'>
+              <Typography>New on our platform?</Typography>
+              <Typography component={Link} color='primary'>
+                Create an account
+              </Typography>
+            </div>
+            <Divider className='gap-3 text-textPrimary'>or</Divider>
+            <div className='flex justify-center items-center gap-2'>
+              <IconButton size='small' className='text-facebook'>
+                <i className='ri-facebook-fill' />
+              </IconButton>
+              <IconButton size='small' className='text-twitter'>
+                <i className='ri-twitter-fill' />
+              </IconButton>
+              <IconButton size='small' className='text-textPrimary'>
+                <i className='ri-github-fill' />
+              </IconButton>
+              <IconButton size='small' className='text-googlePlus'>
+                <i className='ri-google-fill' />
+              </IconButton>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginV2
